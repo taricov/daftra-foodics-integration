c05f7f8a7e61566d768a6f42ad981b93149a0e01
TaricOv <tareq.ouf@gmail.com>
2023-05-19 06:19:14 +0300

    added the next app

---
+++ apps/daftra-foodics-app/app/page.tsx
@@ -0,0 +1,10 @@
+export default async function Index() {
+
+  return (
+    <>
+        <div className="container mx-auto">
+          <div className="text-4xl font-bold">hello</div>
+        </div>
+    </>
+  );
+}
